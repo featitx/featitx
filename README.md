4ebfb421373b72e1c4c382590d1c32268735e947
featitx <siddabdul7@gmail.com>
2023-12-30 15:54:57 +0530

    Adding up initial commit for my git portfolio

---
+++ README.md
@@ -0,0 +1,2 @@
+# portfolio
+PORTFOLIO PROJECT is a dynamic and visually appealing portfolio showcasing my skills, projects, and expertise. Crafted with precision using React for the front end and Sanity as the content management system, this portfolio represents a seamless blend of technology and creativity.
---
+++ sanity_backend/schemas/index.js
@@ -0,0 +1,14 @@
+import testimonal from "./testimonal";
+import brands from "./brands"
+import contact from "./contact";
+import experiences from "./experiences";
+import skills from "./skills";
+import works from "./works";
+import workExperince from "./workExperience";
+
+
+
+export const schemaTypes = [
+    testimonal, brands ,contact, experiences , skills, works , workExperince 
+    
+]
