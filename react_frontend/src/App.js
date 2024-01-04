3d496e9b7cdb44e6efabfafa355541d8adddbe29
featitx <siddabdul7@gmail.com>
2024-01-05 02:58:36 +0530

    pre-final project

--- react_frontend/src/App.js
+++ react_frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {About, Footer , Skills , Testimonial, Header } from './containers/exports';
+import {About, Footer ,Work, Skills , Testimonial, Header } from './containers/exports';
 import Navbar from './component/Navbar/Navbar';
 import './App.scss'
 
@@ -10,6 +10,7 @@ const App = () =>{
             <Navbar></Navbar>
             <Header></Header>
             <About></About>
+             <Work></Work>
             <Skills></Skills>
             <Testimonial></Testimonial>
             <Footer></Footer>
